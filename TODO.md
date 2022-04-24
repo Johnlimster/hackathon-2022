98aae7f25e46e42ad5377094481b2d8a63ac8df7
Jason Han <jjhan@vassar.edu>
2022-04-23 23:51:59 -0400

    Add missing files: content/autofill.js and TODO.md

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO
+
+1. Block duplicates
+2. If no input has `autocomplete` but there is still a form present, fall back
+to pop-up input method
+3. Make detection happen upon typing instead of page load
---
+++ content/autofill.js
@@ -0,0 +1,39 @@
+let hasEmailField = false;
+let hasDobField = false;
+let hasAddressField = false;
+
+let addressFields = [
+    "street-address",
+    "address-line1",
+    "address-line2",
+    "address-line3",
+];
+
+let inputEls = document.querySelectorAll("input[autocomplete]");
+inputEls.forEach((inputEl) => {
+    let autofillDetails;
+    if (inputEl.autocomplete == "on") autofillDetails = inputEl.name;
+    else autofillDetails = inputEl.autocomplete;
+    inputEl.placeholder = autofillDetails; // TODO: remove this
+
+    let fieldType = fieldTypeFromAutofillDetails(autofillDetails);
+    if (fieldType == "email") hasEmailField = true;
+    else if (fieldType == "bday") hasDobField = true;
+    else if (fieldType == "address") hasAddressField = true;
+});
+
+chrome.runtime.sendMessage(
+    { hasEmailField, hasDobField, hasAddressField },
+    function (response) {
+        console.log(response.farewell);
+    }
+);
+
+function fieldTypeFromAutofillDetails(autofillDetails) {
+    let detailsList = autofillDetails.split(" ");
+    if (detailsList.includes("email")) return "email";
+    else if (detailsList.includes("bday")) return "bday";
+    else if (addressFields.some((value) => detailsList.includes(value)))
+        return "address";
+    else return "n/a";
+}
