b47e03dc87dfa6f4b90a3f5a765c987d0a212834
Jason Han <jjhan@vassar.edu>
2022-04-23 23:45:04 -0400

    Allow communication between content and background scripts

--- background.js
+++ background.js
@@ -4,3 +4,44 @@ chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ color });
     console.log("Default background color set to %cgreen", `color: ${color}`);
 });
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    storeInfo(request, sender).then(() => {
+        sendResponse({ farewell: "goodbye" });
+    });
+    return true;
+});
+
+async function storeInfo(request, sender) {
+    let website = sender.tab.url;
+    let email = request.hasEmailField;
+    let dob = request.hasDobField;
+    let address = request.hasAddressField;
+
+    let oldWebsites = await getStorageByKey("websites");
+    let oldEmails = await getStorageByKey("emails");
+    let oldDobs = await getStorageByKey("dobs");
+    let oldAddresses = await getStorageByKey("addresses");
+    oldWebsites.push(website);
+    oldEmails.push(email);
+    oldDobs.push(dob);
+    oldAddresses.push(address);
+
+    chrome.storage.sync.set({
+        websites: oldWebsites,
+        emails: oldEmails,
+        dobs: oldDobs,
+        addresses: oldAddresses,
+    });
+}
+
+function getStorageByKey(key) {
+    return new Promise((resolve, reject) => {
+        chrome.storage.sync.get({ [key]: [] }, ({ [key]: result }) => {
+            if (chrome.runtime.lastError) {
+                return reject(chrome.runtime.lastError);
+            }
+            resolve(result);
+        });
+    });
+}
